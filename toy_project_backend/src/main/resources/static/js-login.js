7224a10416bff3f0d3f023f79dbca66c0cb5c3d9
aryu1217 <ryu200112@gmail.com>
2024-09-24 22:49:25 +0900

    add nodejs

---
+++ toy_project_backend/src/main/resources/static/js-login.js
@@ -0,0 +1,39 @@
+function calStartDay(startDate) {
+  const curDate = new Date();
+  const temp = curDate - new Date(startDate);
+  const daysDiffernce = temp / (1000 * 60 * 60 * 24);
+  return Math.floor(daysDiffernce);
+}
+
+let startDate = localStorage.getItem('projectStartDate');
+
+if (!startDate) {
+  startDate = new Date();
+  localStorage.setItem('projectStartDate', startDate);
+}
+
+const dayPassed = calStartDay(startDate);
+
+alert('토이 프로젝트 ' + (dayPassed + 1) + '일차');
+
+function changeColor(buttonId) {
+  const signWithDiv = document.querySelector('.sign-with');
+  switch (buttonId) {
+    case 'kakao':
+      signWithDiv.style.backgroundColor = '#ffe812'; // 카카오 색상
+      break;
+    case 'google':
+      signWithDiv.style.backgroundColor = '#FFFFFF'; // 구글 색상
+      break;
+    case 'github':
+      signWithDiv.style.backgroundColor = '#040404'; // 깃허브 색상
+      break;
+    default:
+      signWithDiv.style.backgroundColor = '';
+  }
+}
+
+function resetColor() {
+  const signWithDiv = document.querySelector('.sign-with');
+  signWithDiv.style.backgroundColor = '';
+}
