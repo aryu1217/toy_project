aa2a80afeea8203a454ec637c303394643bf175b
aryu1217 <ryu200112@gmail.com>
2024-09-24 22:50:48 +0900

    add resgister function

--- js-login.js
+++ js-login.js
@@ -1,20 +1,20 @@
-function calStartDay(startDate) {
-  const curDate = new Date();
-  const temp = curDate - new Date(startDate);
-  const daysDiffernce = temp / (1000 * 60 * 60 * 24);
-  return Math.floor(daysDiffernce);
-}
+// function calStartDay(startDate) {
+//   const curDate = new Date();
+//   const temp = curDate - new Date(startDate);
+//   const daysDiffernce = temp / (1000 * 60 * 60 * 24);
+//   return Math.floor(daysDiffernce);
+// }
 
-let startDate = localStorage.getItem('projectStartDate');
+// let startDate = localStorage.getItem('projectStartDate');
 
-if (!startDate) {
-  startDate = new Date();
-  localStorage.setItem('projectStartDate', startDate);
-}
+// if (!startDate) {
+//   startDate = new Date();
+//   localStorage.setItem('projectStartDate', startDate);
+// }
 
-const dayPassed = calStartDay(startDate);
+// const dayPassed = calStartDay(startDate);
 
-alert('토이 프로젝트 ' + (dayPassed + 1) + '일차');
+// alert('토이 프로젝트 ' + (dayPassed + 1) + '일차');
 
 function changeColor(buttonId) {
   const signWithDiv = document.querySelector('.sign-with');
